3463468a1a99e8d598f2b201b123065c51fe8dc9
Ramneek Kalra <kalraramneekhmr@gmail.com>
2023-03-02 20:07:53 +0530

    Updated Code with AL2 Dependency

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AWS Elastic Beanstalk Express Sample App Instrumented for use with AWS X-Ray
+# AWS Elastic Beanstalk Express Sample App
 This sample application uses the [Express](https://expressjs.com/) framework and [Bootstrap](http://getbootstrap.com/) to build a simple, scalable customer signup form that is deployed to [AWS Elastic Beanstalk](http://aws.amazon.com/elasticbeanstalk/). The application stores data in [Amazon DynamoDB](http://aws.amazon.com/dynamodb/) and publishes notifications to the [Amazon Simple Notification Service (SNS)](http://aws.amazon.com/sns/) when a customer fills out the form.
 
 ## Features
--- app.js
+++ app.js
@@ -23,43 +23,29 @@ if (cluster.isMaster) {
 
 // Code to run if we're in a worker process
 } else {
-    // Include the AWS X-Ray Node.js SDK and set configuration
-    var XRay = require('aws-xray-sdk');
-    var AWS = XRay.captureAWS(require('aws-sdk'));
-    var http = XRay.captureHTTPs(require('http'));
+    var AWS = require('aws-sdk');
     var express = require('express');
     var bodyParser = require('body-parser');
-    var queryString = require('querystring');
 
     AWS.config.region = process.env.REGION
 
-    XRay.config([XRay.plugins.EC2Plugin, XRay.plugins.ElasticBeanstalkPlugin]);
-    XRay.middleware.setSamplingRules('sampling-rules.json');
-    XRay.middleware.enableDynamicNaming();
-
-    var app = express();
     var sns = new AWS.SNS();
     var ddb = new AWS.DynamoDB();
-    var ddbTable = process.env.STARTUP_SIGNUP_TABLE;
-    var snsTopic = process.env.NEW_SIGNUP_TOPIC;
-    var apiCNAME = process.env.API_CNAME || 'localhost';
+
+    var ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
+    var snsTopic =  process.env.NEW_SIGNUP_TOPIC;
+    var app = express();
 
     app.set('view engine', 'ejs');
     app.set('views', __dirname + '/views');
     app.use(bodyParser.urlencoded({extended:false}));
-    app.use(XRay.express.openSegment('myfrontend'));
 
     app.get('/', function(req, res) {
-        XRay.captureAsyncFunc('Page Render', function(seg) {
-            res.render('index', {
-                static_path: 'static',
-                theme: process.env.THEME || 'flatly',
-                flask_debug: process.env.FLASK_DEBUG || 'false'
-            });
-            seg.close();
+        res.render('index', {
+            static_path: 'static',
+            theme: process.env.THEME || 'flatly',
+            flask_debug: process.env.FLASK_DEBUG || 'false'
         });
-        
-        res.status(200).end();
     });
 
     app.post('/signup', function(req, res) {
@@ -70,22 +56,20 @@ if (cluster.isMaster) {
             'theme': {'S': req.body.theme}
         };
 
-        var seg = XRay.getSegment();
-        seg.addAnnotation('email', req.body.email);
-        seg.addAnnotation('theme', req.body.theme);
-        seg.addAnnotation('previewAccess', req.body.previewAccess);
-
         ddb.putItem({
             'TableName': ddbTable,
             'Item': item,
             'Expected': { email: { Exists: false } }        
         }, function(err, data) {
             if (err) {
+                var returnStatus = 500;
+
                 if (err.code === 'ConditionalCheckFailedException') {
-                    res.status(409).end("User already exists");
-                } else {
-                    res.status(500).end("DDB Error");
+                    returnStatus = 409;
                 }
+
+                res.status(returnStatus).end();
+                console.log('DDB Error: ' + err);
             } else {
                 sns.publish({
                     'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email 
@@ -95,57 +79,15 @@ if (cluster.isMaster) {
                     'TopicArn': snsTopic
                 }, function(err, data) {
                     if (err) {
-                        res.status(500).end("SNS Error");
+                        res.status(500).end();
+                        console.log('SNS Error: ' + err);
                     } else {
-                        res.status(201).end("Success");
+                        res.status(201).end();
                     }
                 });            
             }
         });
     });
-
-    app.post('/remoteSignup', function(req, res) {
-        var seg = XRay.getSegment();
-        seg.addAnnotation('theme', req.body.theme);
-        seg.addAnnotation('previewAccess', req.body.previewAccess);
-
-        var reqData = queryString.stringify(req.body);
-
-        var options = {
-            host: apiCNAME,
-            port: '80',
-            path: '/signup',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': Buffer.byteLength(reqData)
-            }
-        };
-
-        // Set up the request
-        var remoteReq = http.request(options, function(remoteRes) {
-            var body = '';
-            remoteRes.setEncoding('utf8');
-            
-            remoteRes.on('data', function(chunk) {
-                body += chunk;
-            });
-
-            remoteRes.on('end', function() {
-                res.status(remoteRes.statusCode).send(body);                
-            });
-        });
-
-        remoteReq.on('error', function(err) {
-            res.status(500).end("Remote error");
-        });
-
-        // post the data
-        remoteReq.write(reqData);
-        remoteReq.end();
-    });
-
-    app.use(XRay.express.closeSegment());
 
     var port = process.env.PORT || 3000;
 
